638f65c3f59be2c6f538115f4f85662484285727
Daniel Bush <danielericbush@live.com>
2025-09-07 18:35:53 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,81 @@
+const { app, BrowserWindow, ipcMain, nativeTheme, Menu } = require('electron/main');
+const path = require('node:path')
+let mainWindow;
+
+//#region APP LEVEL methods
+function createMainWindow() 
+{
+    mainWindow = new BrowserWindow(
+        {
+            width: 800,
+            height: 600,
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js')
+              }
+        }
+    )  
+    mainWindow.loadFile('mainWindow.html');
+
+    // Quit app when closed
+    mainWindow.on('closed', function()
+    {
+        app.quit();
+    });
+}
+
+app.whenReady().then(() =>
+    {
+        createMainWindow();
+
+        app.on('activate', () => 
+            {
+                if (BrowserWindow.getAllWindows().length === 0) 
+                {
+                    createMainWindow();
+                }
+            }
+        )
+        
+        // Build menu from template
+        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    
+        // Insert menu
+        Menu.setApplicationMenu(mainMenu);
+    }
+)
+
+app.on('window-all-closed', () =>
+    {
+        if (process.platform !== 'darwin') app.quit;
+    }
+)
+//#endregion
+  
+//#region MENU TEMPLATES
+const mainMenuTemplate =  [
+    // Each object is a dropdown
+    {
+      label: 'File',
+      submenu:[
+        {
+          label:'Add Item',
+          click(){
+            createAddWindow();
+          }
+        },
+        {
+          label:'Clear Items',
+          click(){
+            mainWindow.webContents.send('item:clear');
+          }
+        },
+        {
+          label: 'Quit',
+          accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+          click(){
+            app.quit();
+          }
+        }
+      ]
+    }
+  ];
---
+++ renderer.js
@@ -0,0 +1,1 @@
+document.getElementById('banner').innerText = "Welcome";
